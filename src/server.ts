07cf4ec7722c0ff333ad748847cde39b5984b1b6
gustmnds <37991797+gustmnds@users.noreply.github.com>
2024-08-23 23:07:19 -0300

    feat: add cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import "express-async-errors";
+import cors from "cors";
 import express from "express";
 import { appRoutes } from "./routes";
 import { WebErrorMiddleware } from "./middlewares/web-error.middleware";
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(appRoutes);
 server.use(WebErrorMiddleware);
